7f976d113ad5ee3ad17f921a4517f8c9186253ed
andre-axt <andreaxtbachtold@gmail.com>
2026-04-15 19:43:05 -0300

    add - error messages in server.c
    Updated error messages to include newline characters for better readability.

--- src/server/server.c
+++ src/server/server.c
@@ -6,8 +6,8 @@
 int8_t set_nonbocking(Socket_t  *sckt) {
 	int flags = fcntl(sckt->socket_fd, F_GETFL, 0);
 	if (flags == -1){
-		char *msg = "Error - fcntl failed";
-		write(1, msg, 21);
+		char *msg = "Error - fcntl failed\n";
+		write(1, msg, 22);
 		return 1;
 	}
 	fcntl->socket_fd = fcntl(sckt_fd, F_SETFL, flags | 0_NONBLOCK); 
@@ -17,14 +17,14 @@ int8_t set_nonbocking(Socket_t  *sckt) {
 int8_t create_server(Socket_t * sckt){
 	sckt->socket_fd = socket(sckt->domain, sckt->type, sckt->protocol);
 	if(sckt->socket_fd < 0){
-		char *msg = "Error - failed to create socket";
-		write(1, msg, 31);
+		char *msg = "Error - failed to create socket\n";
+		write(1, msg, 32);
 		return 1;
 	}
 	if(bind(sckt->socket_fd, (struct sockaddr *) sckt->address, sizeof(sckt->address)) < 0) {
-		char *msg = "Error - failed to bind socket";
-                write(1, msg, 29);
-                return 1;	
+		char *msg = "Error - failed to bind socket\n";
+        write(1, msg, 30);
+        return 1;	
 
 	}
 	return 0;
@@ -36,8 +36,8 @@ int8_t start_listen(Socket_t * sckt){
 	setsockopt(sckt->socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
 	if(listen(sckt->socket_fd, sckt->backlog) < 0){
-		char *msg = "Error - failed to listen socket";
-                write(1, msg, 31);
+		char *msg = "Error - failed to listen socket\n";
+                write(1, msg, 32);
                 return 1;
 	
 	}
@@ -50,7 +50,8 @@ int8_t accept_new_connection(Connection_t *conn){
 	socklen_t client_len = sizeof(client_addr);
 	conn->client_fd = accept(conn->server_fd, *client_addr, client_len); 
 	if(conn->client_fd == -1){
-		char *msg = "Error - accept returned -1";
+		char *msg = "Error - accept returned -1\n";
+		write(
 		return 1;	
 	}
 	return 0;
@@ -65,11 +66,15 @@ int8_t read_client(Connection_t *conn){
 		if (total_read + 4096 > conn->buffer_len){
 			size_t new_size = conn->buffer_size *2;
 			char *new_buffer = realloc(conn->buffer, new_size);
-			if(!new_buffer) return -1;
+			if(!new_buffer){
+				char *msg = "Error - realloc\n";
+				write(1, msg, 17); 
+				return -1;
+			} 
 
 			conn->buffer = new_buffer;
 			conn->buffer_size = new_size;
-			
+			printf("New Buffer Size - %zu\n", new_size);
 		}
 		bytes_read = recv(conn->client_fd, conn->buffer + total_read, conn->buffer_len - total_read, 0);
 		if(bytes_read <= 0){
