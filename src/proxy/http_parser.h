566ef72f5215f5c16966078af4b7f451b689090a
andre-axt <andreaxtbachtold@gmail.com>
2026-04-22 11:14:19 -0300

    add - memory management and parsing functions
    Added functions to free HTTP request and response structures, trim strings, and parse headers.

--- src/proxy/http_parser.h
+++ src/proxy/http_parser.h
@@ -1,6 +1,6 @@
 #ifndef HTTP_PARSE
 #define HTTP_PARSE
-
+#include <stdint.h>
 typedef struct{
 	int status_code;
 	char *version;
@@ -26,6 +26,11 @@ typedef struct{
 
 http_request_t* init_http_request();
 http_response_t* init_http_response();
+void free_request(http_request_t *req);
+void free_response(http_response_t *res);
+char *trim(char *str);
+int8_t parse_headers(char *buffer, char **headers, int *header_count);
+char *get_header(char **headers, int header_count, const char *name);
 void parser_query_string(char *path, char **query_string, char **clean_path);
 http_response_t* response_parser(http_response_t *response, char *buffer);
 http_request_t* request_parser(http_request_t *request, char *buffer);
