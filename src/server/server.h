eef87bb155f61ad849eb8cf011d6f3ec9e177066
andre-axt <andreaxtbachtold@gmail.com>
2026-04-30 20:09:58 -0300

    Add - function to get IP from hostname

--- src/server/server.h
+++ src/server/server.h
@@ -34,6 +34,7 @@ int8_t start_listen(Socket_t * sckt);
 int8_t close_server(Socket_t * sckt);
 int8_t set_nonbocking(Socket_t  *sckt);
 int8_t accept_new_connection(Connection_t *conn);
+char* get_ip_from_host(const char* hostname);
 int8_t read_client(Connection_t *conn);
 int8_t read_buffer(Connection_t *conn);
 
