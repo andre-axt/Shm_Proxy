3d0b56b32f1a95ca5779cc46203d0165539c1c92
andre-axt <andreaxtbachtold@gmail.com>
2026-05-01 22:06:06 -0300

    edit - Fix memory allocation and event handling in main.c
    Fixed memory allocation and pointer initialization for server and cache. Corrected logical errors in event handling and connection management.

--- src/main.c
+++ src/main.c
@@ -2,16 +2,15 @@
 #include "shm_cache.h"
 
 int main(){
-	Socket_t *server;
-	Connection_t * conn;
+	Socket_t *server = malloc(sizeof(Socket_t));
 	server->domain = AF_INET;
 	server->type = SOCK_STREAM;
 	server->protocol = 0;
 	server->address.sin_family = AF_INET;
 	server->address.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(PORT);
+	server->address.sin_port = htons(PORT);
 
-	Cache_t *cache;
+	Cache_t *cache = malloc(sizeof(Cache_t));
 	int epfd = epoll_create1(0);
 	if(create_server(server)){
 		return 1;
@@ -22,7 +21,8 @@ int main(){
 		return 1;
 	
 	}
-       	struct epoll_event ev, events[MAX_EVENTS]; 	
+	
+    struct epoll_event ev, events[MAX_EVENTS]; 	
 	if(start_listen(server)){
 		return 1;
 	}
@@ -32,18 +32,26 @@ int main(){
 	Connection_t * conn = malloc(sizeof(Connection_t);
 	conn->buffer = malloc(BUFFER_SIZE);
 	conn->buffer_len = (size_t) BUFFER_SIZE;
+	conn->remote_server_fd = -1;
+	
 	while(1){
 		int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
 	       	
-		for (int i = 0; i <= MAX_EVENTS; i++){
+		for (int i = 0; i < nfds; i++){
 			
-			if(events[i].data.fd = server->socket_fd){
+			if(events[i].data.fd == server->socket_fd){
 				accept_new_connection(conn, server->socket_fd);
+				ev.events = EPOLLIN | EPOLLET;
+				ev.data.fd = conn->client_fd;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, conn->client_fd, &ev);
 
+			}
+			else if(events[1].data.fd == conn->remote_server_fd) {
+				
 			}
 			else if(events[i].data.fd > 0){
 				conn->client_fd = events[i].data.fd;
-				if(read_client(conn) = 0){
+				if(read_client(conn) == 0){
 					if(conn->buffer > 0){
 						switch(read_buffer(conn)) {
 							case 0:
@@ -54,7 +62,9 @@ int main(){
 								const char* host = get_headers(conn->res->headers, conn->res->header_count, "Host:");
 								if(host){
 									char *ip = get_ip_from_host(host);
-									
+									if(ip) {
+										co
+									}
 								}
 						}
 						
