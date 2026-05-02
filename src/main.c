b20cf48106b21073495d0d8eb4cff73a4b342262
andre-axt <andreaxtbachtold@gmail.com>
2026-05-02 13:28:37 -0300

    Refactor - server connection management
    Refactor connection handling to use ConnectionManager and improve accept logic.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "server.h"
 #include "shm_cache.h"
 
+#define MAX_CONNECTIONS 1000
+
 int main(){
 	Socket_t *server = malloc(sizeof(Socket_t));
 	server->domain = AF_INET;
@@ -16,7 +18,7 @@ int main(){
 		return 1;
 
 	}
-	init_cache(cache)
+	init_cache(cache);
 	if(set_nonblocking(server)){
 		return 1;
 	
@@ -29,10 +31,7 @@ int main(){
 	ev.events = EPOLLIN;
 	ev.data.fd = server->socket_fd;
 	epoll_ctl(epfd, EPOLL_CTL_ADD, server->socket_fd, &ev);
-	Connection_t * conn = malloc(sizeof(Connection_t);
-	conn->buffer = malloc(BUFFER_SIZE);
-	conn->buffer_len = (size_t) BUFFER_SIZE;
-	conn->remote_server_fd = -1;
+	ConnectionManager_t *conn_manager = init_connection_manager(MAX_CONNECTIONS, epfd);
 	
 	while(1){
 		int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
@@ -40,14 +39,18 @@ int main(){
 		for (int i = 0; i < nfds; i++){
 			
 			if(events[i].data.fd == server->socket_fd){
-				accept_new_connection(conn, server->socket_fd);
-				ev.events = EPOLLIN | EPOLLET;
-				ev.data.fd = conn->client_fd;
-				epoll_ctl(epfd, EPOLL_CTL_ADD, conn->client_fd, &ev);
+				struct sockaddr_in client_addr;
+				socklen_t addr_len = sizeof(client_addr);
+				int client_fd = accept(server->socket_fd, (struct sockaddr*)&client_addr, &addr_len);
 
-			}
-			else if(events[1].data.fd == conn->remote_server_fd) {
-				
+				if(client_fd > 0) {
+					set_nonblocking(client_fd);
+					int8_t idx = add_client_connection(conn_manager, client_fd);
+					if(idx == -1) {
+						close(client_fd);
+					}
+				}
+
 			}
 			else if(events[i].data.fd > 0){
 				conn->client_fd = events[i].data.fd;
