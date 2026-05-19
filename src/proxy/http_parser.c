3c5696db21d11a15b19b81ff5e2ce53a5be30b22
andre-axt <andreaxtbachtold@gmail.com>
2026-05-19 15:54:02 -0300

    Use - calloc for initializing HTTP request and response

--- src/proxy/http_parser.c
+++ src/proxy/http_parser.c
@@ -6,26 +6,13 @@
 #include <unistd.h>
 
 http_request_t* init_http_request(){
-        http_request_t *req = malloc(sizeof(http_request_t));
-        req->method = NULL;
-        req->path = NULL;
-        req->version = NULL;
-        req->headers = NULL;
-        req->header_count = 0;
-        req->body = NULL;
-        req->body_length = 0;
+        http_request_t *req = calloc(1, sizeof(http_request_t));
         return req;
 
 }
 
 http_response_t* init_http_response(){
-        http_response_t *res = malloc(sizeof(http_response_t));
-        res->status_code = 0;
-        res->version = NULL;
-        res->headers = NULL;
-        res->header_count = 0;
-        res->body = NULL;
-        res->body_length = 0;
+        http_response_t *res = calloc(1, sizeof(http_response_t));
         return res;
         
 }
